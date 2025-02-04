c12c435aa2ce244508eb4c3b56ec8ed4d8f10b43
MmDZ21 <mmohamadzz294@gmail.comgit config --global user.name MmDZ21git config --global user.email mmohamadzz294@gmail.com>
2025-02-04 13:51:14 +0330

    fix routes

--- app/api/getToken/route.ts
+++
@@ -1,7 +0,0 @@
-// import { auth } from "@/auth";
-// import { NextApiRequest, NextApiResponse } from "next";
-
-// export async function GET(req: NextApiRequest, res: NextApiResponse) {
-//   const token = await auth(req, res);
-//   return console.log(token);
-// }
