27cc4acda27c39ff7eba15a4492b9f649fcf0156
MmDZ21 <mmohamadzz294@gmail.comgit config --global user.name MmDZ21git config --global user.email mmohamadzz294@gmail.com>
2025-02-05 14:00:48 +0330

    fix redirect on signin

--- actions/login.ts
+++ actions/login.ts
@@ -3,11 +3,7 @@
 import { signIn } from "@/auth";
 
 export default async function login(formData: FormData) {
-    console.log("formdata: ", formData)
-   formData.append('redirectTo', '/dashboard');
-    try {
-      await signIn("credentials", formData)
-    } catch (error) {
-      console.log("login error: " + error)
-    }
-  }
+  console.log("formdata: ", formData);
+  formData.append("redirectTo", "/dashboard");
+  await signIn("credentials", formData);
+}
